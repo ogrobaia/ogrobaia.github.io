e2aa569f67a506c76b819859608587025858c50e
serverless <157746369+ogrobaia@users.noreply.github.com>
2024-01-29 17:29:56 -0300

    atualização < retirada das imagens e inserção de planilhas google>

--- gestao.py
+++ gestao.py
@@ -6,43 +6,32 @@ app = Flask(__name__)
 # Pasta onde as imagens estão localizadas
 image_folder = "static"
 
-# Mapeamento de nomes de colunas para nomes de imagens
-column_image_mapping = {
-    "Mês": "mês.png",
-    "Loja": "loja.png",
-    "Turno": "turno.png",
-    "Vigilante": "vigilante.png",
-    "Fiscal": "fiscal.png",
-    "Setor": "setor.png",
-    "Valor": "valor.png",
-    "Classificação": "classificação.png",
+# Mapeamento de nomes de colunas para URLs dos gráficos
+column_chart_mapping = {
+    "Mês": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=1643534116&format=interactive",
+    "Loja": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=693678060&format=interactive",
+    "Turno": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=599875144&format=interactive",
+    "Vigilante": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=1836405538&format=interactive",
+    "Fiscal": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
+    "Setor": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
+    "Valor": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
+    "Classificação": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
 }
-
-
-# Função para obter a lista de nomes de arquivos de imagens na pasta
-def get_image_files():
-    image_files = []
-    for filename in os.listdir(image_folder):
-        if filename.endswith(".png"):
-            image_files.append(filename)
-    return image_files
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    image_files = get_image_files()
-    selected_image = None
+    selected_chart = None
 
     if request.method == "POST":
         selected_column = request.form["seletor"]
-        if selected_column in column_image_mapping:
-            selected_image = column_image_mapping[selected_column]
+        if selected_column in column_chart_mapping:
+            selected_chart = column_chart_mapping[selected_column]
 
     return render_template(
         "index.html",
-        column_image_mapping=column_image_mapping,
-        image_files=image_files,
-        selected_image=selected_image,
+        column_chart_mapping=column_chart_mapping,
+        selected_chart=selected_chart,
     )
 
 
