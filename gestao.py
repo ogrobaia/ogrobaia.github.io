d358bdb99152c1bc5f5b966e068bdd1a33ae8989
serverless <157746369+ogrobaia@users.noreply.github.com>
2024-02-16 17:37:33 -0300

    modificando exibição dos graficos gestao.py
    retiradas dos botões para exibição diretamente na pagina

--- gestao.py
+++ gestao.py
@@ -1,40 +1,167 @@
-from flask import Flask, render_template, request
-import os
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestão de Desempenho - Relatório de Ações de Segurança RJ</title>
+    <style>
+        body {
+            background-color: #f0f7ff;
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
 
-app = Flask(__name__)
+        #logo {
+            width: 100px;
+            height: auto;
+            margin: 20px;
+            animation: destacarLogo 3s infinite alternate;
+        }
 
-# Pasta onde as imagens estão localizadas
-image_folder = "static"
+        @keyframes destacarLogo {
+            0% {
+                transform: scale(1);
+            }
+            100% {
+                transform: scale(1.2);
+            }
+        }
 
-# Mapeamento de nomes de colunas para URLs dos gráficos
-column_chart_mapping = {
-    "Data": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=500388460&amp;format=interactive",
-    "Mês": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=1643534116&format=interactive",
-    "Loja": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=693678060&format=interactive",
-    "Turno": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=599875144&format=interactive",
-    "Vigilante": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=1836405538&format=interactive",
-    "Fiscal": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
-    "Setor": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
-    "Valor": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
-    "Classificação": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
-}
+        .container {
+            max-width: 90%;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 16px;
+            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+            text-align: left;
+        }
 
+        .link {
+            margin-bottom: 10px;
+            padding: 15px 0;
+            font-size: 16px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+            text-align: center;
+            display: block;
+            text-decoration: none;
+            text-align: center;
+            width: fit-content;
+            margin: 10px auto;
+        }
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    selected_chart = None
+        .link:hover {
+            background-color: #45a049;
+        }
 
-    if request.method == "POST":
-        selected_column = request.form["seletor"]
-        if selected_column in column_chart_mapping:
-            selected_chart = column_chart_mapping[selected_column]
+        .conteudo-principal {
+            text-align: center;
+        }
 
-    return render_template(
-        "index.html",
-        column_chart_mapping=column_chart_mapping,
-        selected_chart=selected_chart,
-    )
+        .texto {
+            margin-top: 20px;
+            text-align: justify;
+        }
 
+        iframe {
+            width: 100%;
+            height: 400px;
+            margin-bottom: 20px;
+        }
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        .rodape {
+            text-align: left;
+        }
+
+        @media screen and (max-width: 768px) {
+            .botao {
+                flex-basis: calc(50% - 10px);
+            }
+        }
+
+        @media screen and (max-width: 480px) {
+            .botao {
+                flex-basis: calc(100% - 10px);
+            }
+        }
+    </style>
+</head>
+<body>
+    <img id="logo" src="https://img.freepik.com/vetores-premium/gire-o-icone-do-smartphone-no-fundo-branco-simbolo-de-rotacao-do-dispositivo-ilustracao-em-vetor-linha_476325-19.jpg" alt="Logo">
+    
+    <div class="container">
+        <div class="links-superiores">
+            <a href="https://www.nagumo.com.br/">Site Nagumo</a>
+            <a href="https://www.youtube.com/c/SupermercadosNagumo">Canal do Youtube</a>
+            <a href="https://www.instagram.com/supermercados_nagumo?igsh=MXFvb3NkanphYmtwOQ==">Instagram</a>
+        </div>
+
+        <div class="container">
+            <h1># Gestão de Desempenho # Relatório de Ações de Segurança RJ</h1>
+            </div>
+
+            <div class="conteudo-principal" id="graficos">
+                <!-- Os gráficos serão adicionados automaticamente aqui -->
+            </div>
+            
+            <div class="texto">
+                <h2>Coleta dos Dados</h2>
+                <p>Utilizando-se de siglas já pré-determinadas para cada ação cometida através de intervenção do agente ou observação das atitudes do elemento a quem se dirige, foram adotadas as seguintes práticas:</p>
+                <p><strong>F.F (Furto Frustrado):</strong> Onde através da ação do agente, na postura ou observando o elemento, seja ele já conhecido por práticas anteriores ou através de abandono da rés furtiva, dá-se a ação de furto frustrado, por abandono ou casos parecidos.</p>
+                <p><strong>F.C (Furto Consumado):</strong> Onde o elemento subtrai o objeto, deixa o estabelecimento e por ação do agente é abordado diretamente.</p>
+                <p><strong>C.A.P (Consumo Acompanhado Pago):</strong> Onde o cliente degusta de diversos tipos de produtos, sendo observado durante toda a sua permanência no interior da loja, para que não abandone o produto deixando de pagar. Vale salientar que tal prática é costumeira e pode vir a causar grande prejuízo à empresa.</p>
+                <p><strong>A.T.F (Atitude Típica de Furto):</strong> Onde o cliente, seja com intenção ou no costume de utilizar bolsa ecológica, coloca todos os produtos em seu interior, seja dentro da bolsa no interior do carrinho de compras, seja na bolsa pendurada em seu braço ou pela utilização de carrinhos de compras particulares, onde este, quando intencionado a praticar furto, deixa a loja por entre os caixas ou até mesmo pela entrada principal, causando prejuízo enorme à empresa. Desta forma, a prática vem sendo monitorada pelos seguranças, fiscais e a frente de loja.</p>
+            </div>
+            
+            <!-- Div para incorporar o formulário do Google -->
+            <div id="google-form">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdJahlGovT2paYj0OK4mRBiLDmsuuvPQUpHC7xfwDMnyYNm6w/viewform?embedded=true" width="100%" height="747" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
+            </div>
+
+            <div class="rodape">
+                <p><strong>Staff</strong></p>
+                <p><strong>Supervisor Regional:</strong> Claudio</p>
+                <p><strong>Supervisor Perdas:</strong> Emerson Carriero</p>
+                <p><strong>Gerente Loja 36:</strong> Miranda</p>
+                <p><strong>Gerente Loja 53:</strong> Henrique</p>
+                <p><strong>Gestor de Perdas:</strong> Daniel</p>
+                <!-- Informações sobre a equipe aqui -->
+            </div>
+        </div>
+    </div>
+
+    <script>
+        var column_chart_mapping = {
+            "Data": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=500388460&amp;format=interactive",
+            "Mês": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=1643534116&format=interactive",
+            "Loja": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=693678060&format=interactive",
+            "Turno": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=599875144&format=interactive",
+            "Vigilante": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=1836405538&format=interactive",
+            "Fiscal": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
+            "Setor": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
+            "Valor": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
+            "Classificação": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLhUU91HJTLP2jwfLkJ1sD8lrhpkVwy8Ea8hEYOx4VCDU5BamiCk0FEawL8gEMN3OFrEyNPTYHvfD/pubchart?oid=391302009&format=interactive",
+        };
+
+        window.onload = function() {
+            var container = document.getElementById("graficos");
+
+            for (var column in column_chart_mapping) {
+                var iframe = document.createElement("iframe");
+                iframe.src = column_chart_mapping[column];
+                iframe.width = "100%";
+                iframe.height = "600";
+                iframe.frameBorder = "10";
+                iframe.scrolling = "no";
+                container.appendChild(iframe);
+            }
+        };
+    </script>
+</body>
+</html>
